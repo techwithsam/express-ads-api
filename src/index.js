e08d23c01a65b81727bfe3b9ebccfa4e3ca6c6f3
Samuel Adekunle <samuelbeebest@gmail.com>
2021-08-09 23:03:08 +0100

    initiating file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('Good evening, Samuel!');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const app = express();
+
+const ads = [{ title: "Hello, world (again)!" }];
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(morgan("combined"));
+
+app.get("/", (req, res) => {
+  res.send(ads);
+});
+
+app.listen(3001, () => {
+  console.log("listening on port 3001");
+});
